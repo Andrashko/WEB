124ba6fae05162ba28e36bb696eb4dcb921249e7
Andrashko <yurii.andrashko@uzhnu.edu.ua>
2022-04-16 16:58:48 +0300

    mkr

--- DOM/mkr/task3.js
+++ DOM/mkr/task3.js
@@ -1,24 +1,25 @@
-document.getElementById("calc-total").onclick = () =>{
+document.getElementById("calc-total").onclick = () => {
     let currency = parseFloat(document.getElementById("currency").value);
     // let shipping = document.querySelector('input[name="shipping"]:checked')?.value;
     let shipping = "";
-    for (let radio of document.getElementsByName("shipping")){
+    for (let radio of document.getElementsByName("shipping")) {
         if (radio.checked)
-            shipping=radio.value;
-    }
-    
-    let emergency = document.getElementById("emergency").checked;
-    if (shipping == "post"){
-        currency += 50
-    } else if (shipping == "new-post"){
-        currency += 70
-    } else if (shipping == "courier"){
-        currency += 100
-    } else {
-        alert ("Спосібдоставки не вибрано");
+            shipping = radio.value;
     }
 
-    if (emergency){
+    let expedited = document.getElementById("expedited").checked;
+
+    if (shipping == "post") {
+        currency += 50
+    } else if (shipping == "new-post") {
+        currency += 70
+    } else if (shipping == "courier") {
+        currency += 100
+    } else {
+        alert("Спосібдоставки не вибрано");
+    }
+
+    if (expedited) {
         currency *= 1.05;
     }
 
